9af89e7561b8f2dbfe3ae302aac1a728edba88fc
Patrik Trefil <patrik.trefil@apify.com>
2022-07-21 17:27:15 +0200

    got http requests working

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,11 @@
-﻿System.Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program {
+    static async Task Main(string[] args) {
+        HttpClient client = new HttpClient();
+        var url = "https://apify.com"; // TODO: get url from input
+        var pageContent = await client.GetStringAsync(url);
+        Console.WriteLine($"Got HTML from URL: {url}");
+        System.Console.WriteLine(pageContent); // TODO: save to OUTPUT.json
+    }
+}
